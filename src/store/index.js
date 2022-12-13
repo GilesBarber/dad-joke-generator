17271e773859c0d1a8e8c49b9b0f682436681895
GilesBarber <34550461+GilesBarber@users.noreply.github.com>
2022-12-13 14:52:41 +0000

    add files

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,32 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const url = "https://icanhazdadjoke.com/"
+const headers = { Accept: "application/json"}
+
 export default new Vuex.Store({
   state: {
+    currentJoke: 'this is not a joke',
+    allJokes: []
   },
-  getters: {
+  getters: { 
+    getCurrentJoke: state => state.currentJoke,
+    getAllJokes: state => state.allJokes
+
   },
-  mutations: {
+  mutations: { // synchronous way to update state in the store
+    setCurrentJoke(state, payload){
+      state.currentJoke = payload;
+      state.allJokes.push(payload)
+    }
   },
-  actions: {
+  actions: { // asynchronous way to 
+    async setCurrentJoke(state){
+      const joke = await fetch(url, { headers })
+      const j = await joke.json()
+      state.commit('setCurrentJoke', j.joke)
+    }
+
   },
   modules: {
   }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Dad joke generator</h1>
+    <p>Gets jokes from https://icanhazdadjoke.com/</p>
+    <p>Uses:</p>
+    <ul>
+      <li>Vue 2</li>
+      <li>Vuex</li>
+      <li>Vuetify</li>
+    </ul>
   </div>
 </template>
+<style scoped>
+h1{
+  color: darkorange;
+  font-weight: bold;
+  font-size: 2.5rem;
+}
+.about{
+margin: 2rem;
+}
+
+</style>
